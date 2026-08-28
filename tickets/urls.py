2326ca003bdfefed138266adf5fcb6b4720ac1dc
burakkzereykaya <gokraptor20@gmail.com>
2026-08-28 13:59:09 +0300

    Add user ticket creation flow

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,5 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     path("tickets/", views.ticket_list, name="ticket_list"),
+    path("tickets/create/",views.create_ticket,name="create_ticket"),
     path("tickets/<int:ticket_id>/",views.ticket_detail,name="ticket_detail"),
+
     ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.decorators import  login_required
 from .models import Ticket
+from .forms import TicketForm
 
 @login_required
 def ticket_list(request):
@@ -22,3 +23,24 @@ def ticket_detail(request, ticket_id):
     }
 
     return render(request,"tickets/ticket_detail.html",context)
+
+@login_required()
+def create_ticket(request):
+    if request.method == "POST":
+        form=TicketForm(request.POST)
+        if form.is_valid():
+            ticket = form.save(commit=False)
+            ticket.reported_by=request.user
+            ticket.recorded_by=request.user
+            ticket.status=Ticket.TicketStatuses.Queued
+            ticket.assigned_to=None
+            ticket.save()
+            form.save_m2m()
+            return redirect("ticket_detail",ticket_id=ticket.pk)
+
+    else:
+        form=TicketForm()
+    context={
+            "form":form
+        }
+    return render(request,"tickets/create_ticket.html",context)
