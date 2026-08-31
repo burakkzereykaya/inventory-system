ce1d6e19df1b159f9dd6af4ac1fa8d4520771ae3
burakkzereykaya <gokraptor20@gmail.com>
2026-08-31 11:51:02 +0300

    Add user ticket actions and comment workflow

--- tickets/views.py
+++ tickets/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.decorators import  login_required
-from .models import Ticket
+from .models import Ticket, TicketAssignmentHistory
 from .forms import TicketForm,TicketCommentForm
 
 @login_required
@@ -17,15 +17,54 @@ def ticket_list(request):
 
 @login_required
 def ticket_detail(request, ticket_id):
-    comment_form=None
+    comment_form = None
     ticket = get_object_or_404(Ticket,pk=ticket_id)
     comments=ticket.comments.all()
-    if request.user.has_perm("tickets.add_ticketcomment"):
-        comment_form=TicketCommentForm()
-    context={
-        "ticket":ticket,
-        "comments":comments,
-        "comment_form":comment_form,
+    can_take_ticket= (
+        request.user.has_perm("tickets.change_ticket")
+        and  ticket.assigned_to is None
+        and  ticket.status != Ticket.TicketStatuses.Resolved
+    )
+
+    if request.method == "POST":
+        if "take_ticket" in request.POST:
+            if can_take_ticket:
+                ticket.assigned_to=request.user
+                ticket.save()
+
+                TicketAssignmentHistory.objects.create(
+                    ticket=ticket,
+                    previous_assignee=None,
+                    new_assignee=request.user,
+                    changed_by=request.user,
+                )
+
+                return redirect("ticket_detail",ticket_id=ticket.pk)
+
+        else:
+                    if request.user.has_perm("tickets.add_ticketcomment"):
+                            comment_form=TicketCommentForm(request.POST)
+                            if comment_form.is_valid():
+                                comment=comment_form.save(commit=False)
+                                comment.ticket=ticket
+                                comment.author=request.user
+                                comment.save()
+
+                                return redirect("ticket_detail",ticket_id=ticket.pk)
+                    else:
+                        comment_form = None
+
+
+    else:
+        if request.user.has_perm("tickets.add_ticketcomment"):
+            comment_form= TicketCommentForm()
+
+
+    context = {
+    "ticket": ticket,
+    "comments": comments,
+   "comment_form": comment_form,
+    "can_take_ticket": can_take_ticket
     }
 
     return render(request,"tickets/ticket_detail.html",context)
