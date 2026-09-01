280e76671c470578b384bf82f58d0a2ca8559b3b
burakkzereykaya <gokraptor20@gmail.com>
2026-09-01 10:06:26 +0300

    Add device detail page

--- locations/urls.py
+++ locations/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("devices/",views.device_list,name="device_list")
-
+    path("devices/",views.device_list,name="device_list"),
+    path("devices/<int:device_id>/",views.device_detail,name="device_detail"),
 ]
--- locations/views.py
+++ locations/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 from locations.models import Device
 
@@ -13,4 +13,15 @@ def device_list(request):
     }
 
     return render(request,"locations/device_list.html",context)
+
+@login_required
+def device_detail(request,device_id):
+
+    device = get_object_or_404(Device,pk=device_id)
+    context={
+        "device":device
+    }
+
+
+    return render(request,"locations/device_detail.html",context)
 
