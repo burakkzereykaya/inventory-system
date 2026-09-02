d8a89a82ffc6b1979f8cd379be6f29f5069d92dc
burakkzereykaya <gokraptor20@gmail.com>
2026-09-02 11:28:45 +0300

    Add device editing and status history workflow

--- locations/urls.py
+++ locations/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns=[
     path("devices/",views.device_list,name="device_list"),
     path("devices/create",views.create_device,name="create_device"),
+    path("devices/<int:device_id>/edit/",views.edit_device,name="edit_device"),
     path("devices/<int:device_id>/",views.device_detail,name="device_detail"),
 ]
--- locations/views.py
+++ locations/views.py
@@ -1,8 +1,10 @@
+from logging import raiseExceptions
+
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 from locations.forms import DeviceForm
-from locations.models import Device
+from locations.models import Device, DeviceStatusHistory
 
 
 @login_required
@@ -52,3 +54,40 @@ def create_device(request):
             "form":form
         }
     return render(request,"locations/create_device.html",context)
+
+
+@login_required
+@permission_required('locations.change_device',raise_exception=True)
+def edit_device(request,device_id):
+    device=get_object_or_404(Device,pk=device_id)
+    previous_status=device.status
+    previous_status_note=device.status_note
+
+    if request.method == "POST":
+        form=DeviceForm(request.POST,instance=device)
+        if form.is_valid():
+            updated_device = form.save(commit=False)
+            updated_device.updated_by = request.user
+
+            status_changed =( previous_status != updated_device.status or previous_status_note != updated_device.status_note)
+            updated_device.save()
+
+
+            if status_changed:
+                DeviceStatusHistory.objects.create(
+        device = updated_device,
+        previous_status = previous_status,
+        new_status = updated_device.status,
+        status_note = updated_device.status_note,
+        changed_by = request.user,
+                )
+
+            return redirect("device_detail", device_id=updated_device.pk)
+    else:
+        form =DeviceForm(instance=device)
+
+    context = {
+        "form": form,
+        "device": device,
+    }
+    return render(request,"locations/edit_device.html",context)
