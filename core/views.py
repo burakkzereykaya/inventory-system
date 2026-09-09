925877a10fbd94d7792eb3f3c5f928ee5cd46dc5
burakkzereykaya <gokraptor20@gmail.com>
2026-09-09 16:16:47 +0300

    Improve dashboard device ticket and form styling

--- core/views.py
+++ core/views.py
@@ -1,6 +1,27 @@
+from itertools import count
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from locations.models import Device
+from tickets.models import Ticket
 
 @login_required
 def dashboard(request):
-    return render(request,"core/dashboard.html")
+
+    total_devices=Device.objects.count()
+
+    active_devices=Device.objects.filter(
+        status=Device.Statuses.Active
+    ).count()
+
+    open_tickets = Ticket.objects.exclude(
+        status=Ticket.TicketStatuses.Resolved
+    ).count()
+
+    context={
+        "total_devices":total_devices,
+        "active_devices":active_devices,
+        "open_tickets":open_tickets,
+    }
+
+    return render(request,"core/dashboard.html",context)
